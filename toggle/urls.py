28dd99f2ad7366cdfa6249db24be56224caaf0a7
YousefDawoad <62189386+Yossef-Dawoad@users.noreply.github.com>
2022-08-06 22:15:57 +0200

    Update urls.py

--- toggle/urls.py
+++ toggle/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.indexview , name='index'),
     path('lock_api/lock-states/', views.listall_states, name='states'),
-    path('api/lock/records', views.listall_states, name='states'),
+    path('api/lock/records/', views.listall_states, name='states'),
     path('lock_api/last-state/', views.last_state, name='last-state'),
-    path('api/lock/last-record', views.last_state, name='last-state'),
+    path('api/lock/last-record/', views.last_state, name='last-state'),
     path('lock_api/create-state/', views.create_state, name='create-state'),
     path('opendoor/', views.open_the_lock)
 ]
